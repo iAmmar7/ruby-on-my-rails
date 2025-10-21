4c178a7477a37a01df535919d1dd8fc01b9102ba
Ammar Ansari <iammaransari@gmail.com>
2025-10-21 15:18:54 +0200

    stock picker implementation

---
+++ stock_picker.rb
@@ -0,0 +1,26 @@
+def stock_picker(stocks)
+  min_price = Float::INFINITY
+  max_profit = 0
+
+  best_buy = 0
+  best_sell = 0
+  min_index = -1
+
+  stocks.each_with_index do |price, i|
+    if price < min_price
+      min_price = price
+      min_index = i
+    else
+      newProfit = price - min_price
+      if newProfit > max_profit
+        max_profit = newProfit
+        best_buy = min_index
+        best_sell = i
+      end
+    end
+  end
+
+  return [best_buy, best_sell]
+end
+
+puts stock_picker([17,3,6,9,15,8,6,1,10])
